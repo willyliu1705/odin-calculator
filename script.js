539c51ca2057f66b048372e69938709ee9632215
William Liu <will.liu1705@gmail.com>
2025-10-28 21:29:31 -0400

    Add logic for four operators
    - add, subtract, multiply, and divie

---
+++ script.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let operand1;
+let operand2;
+let operator;
+
+function operate(operand1, operand2, operator) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(operand1, operand2);
+      break;
+    case "-":
+      result = subtract(operand1, operand2);
+      break;
+    case "*":
+      result = multiply(operand1, operand2);
+      break;
+    case "/":
+      result = divide(operand1, operand2);
+      break;
+    default:
+      result = NaN;
+  }
+  return result;
+}
