cc68b8d66709a0efaaf04482ad0a58dde2d65096
William Liu <will.liu1705@gmail.com>
2025-10-29 08:26:14 -0400

    Make digit buttons responsive in the display
    - still need to implement the equals and AC buttons
    - grouped buttons by classes according to their functionality

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ function divide(a, b) {
 let operand1;
 let operand2;
 let operator;
+let operandOneFilled = false;
+// TODO: need to also add operandTwFilled so I know when to evaluate the pair of numbers
 
 function operate(operand1, operand2, operator) {
   let result;
@@ -38,3 +40,37 @@ function operate(operand1, operand2, operator) {
   }
   return result;
 }
+
+let displayDiv = document.querySelector("#display div");
+let digitButtons = document.querySelectorAll(".digits");
+let operatorButtons = document.querySelectorAll(".operator");
+
+digitButtons.forEach((digitButton) => {
+  digitButton.addEventListener("click", (event) => {
+    let buttonContent = event.target.textContent;
+    displayDiv.textContent += buttonContent;
+
+    if (!operandOneFilled) {
+      operand1 = Number(buttonContent);
+    } else {
+      operand2 = Number(buttonContent);
+    }
+  })
+})
+
+operatorButtons.forEach((operatorButton) => {
+  operatorButton.addEventListener("click", (event) => {
+    let buttonContent = event.target.textContent;
+    switch (buttonContent) {
+      case "AC":
+        // TODO
+        break;
+      case "=":
+        // TODO
+        break;
+      default:
+        operator = buttonContent;
+    }
+  })
+})
+
